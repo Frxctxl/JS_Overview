81aee6420538d744af12ac181736e23637b929a1
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-06-20 12:56:55 -0400

    loop practice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-var firstName = 'Jayden';
-var age = 21;
+var names = ['John', 'Anne', 'Mark', 'Avi', 'Faith'];
+var users = ['Avi', 21, 'Faith', 31, 'Jerry', 58];;
 
-console.log(firstName + ' ' + age);
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
