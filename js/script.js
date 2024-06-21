ad513e817d23bb6450d757a62da1fca1eeb105cb
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-06-21 13:41:12 -0400

    finished object basics

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-var firstName = 'Jayden';
-var age = 21;
+const str = 'some string';
+const num = 10;
+const bool = true;
+const arr = ['Jay', 'Avi', 'Faith'];
 
-console.log(firstName + ' ' + age);
+const data = {
+  name: 'JD',
+  age: 44,
+  info: {
+    location: 'Atl',
+    hobbies: [
+      {
+        name: 'Fishing',
+        frequency: 'once every two weeks'
+      },
+      {
+        name: 'Pickleball',
+        frequency: 'once a week'
+      }
+    ]
+  }
+};
+
+console.log(data.info.hobbies[0].frequency);
