48a7c17d6f83fe7cbe4d047ed7369dea8f507ccc
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-06-21 12:39:55 -0400

    finished calc function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-var firstName = 'Jayden';
-var age = 21;
+/*Create a function with 3 args
+* 1st and 2nd are numbers
+* 3rd is operation
+ */
 
-console.log(firstName + ' ' + age);
+function calc(num1, num2, operation) {
+  //checks each operation and does required math
+  if (operation === '+') {
+    return num1 + num2;
+  }
+  else if (operation === '-') {
+    return num1 - num2;
+  }
+  else if (operation === '*') {
+    return num1 * num2;
+  }
+  else if (operation === '/') {
+    return num1 / num2;
+  }
+  //if no operation is done return an error message
+  else {
+    return 'Invalid operation'
+  }
+}
+
+
+console.log(calc(39, 21, '/'));
