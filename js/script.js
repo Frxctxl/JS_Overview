e089ffc75e8811ead968b2c7dc5c2676144f9f52
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-06-24 14:35:13 -0400

    notes beta

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
-var firstName = 'Jayden';
-var age = 21;
+//Target the form 
+const noteForm = document.querySelector('#noteForm');
+//Target the note header h3
+const noteHead = document.querySelector('.noteHeader');
 
-console.log(firstName + ' ' + age);
+noteForm.style.background = '#9FAFDF';
+
+function createNote(eventObj) {
+  eventObj.preventDefault();
+
+  const noteInput = document.querySelector('#noteInput').value;
+
+  const dateObj = new Date();
+  const month = dateObj.getMonth() + 1;
+  const date = dateObj.getDate();
+  const year = dateObj.getFullYear();
+  const dateStr = `${month}/${date}/${year}`;
+
+  const note = {
+    text: noteInput,
+    date: dateStr
+  }
+
+  const notes = JSON.parse(localStorage.getItem('notes')) || [];
+  notes.push(note);
+
+  localStorage.setItem('notes', JSON.stringify(notes));
+}
+noteForm.addEventListener('submit', createNote);
+
+// localStorage.setItem('name', 'Jay');
