77f77f8fb510fd23311c73372c9d27368faf2127
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-06-27 13:23:15 -0400

    finished-multi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-var firstName = 'Jayden';
-var age = 21;
+const lis = document.querySelectorAll('ul li');
+const output = document.querySelector('#output');
 
-console.log(firstName + ' ' + age);
+function doSomething(eventObj) {
+
+  output.innerText = eventObj.target.innerText;
+
+  lis.forEach(function (li) {
+    li.style.backgroundColor = 'gray';
+  });
+
+  eventObj.target.style.backgroundColor = 'red';
+}
+for (const li of lis) {
+  li.addEventListener('click', doSomething);
+}
