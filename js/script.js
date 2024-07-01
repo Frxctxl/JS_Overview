7806d207119c8825ac2403ba5e63a38688846a0b
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-07-01 11:52:35 -0400

    finished requests

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-var firstName = 'Jayden';
-var age = 21;
+const apiKey = '483a53306eacda4704b24e0d2bddcae4';
 
-console.log(firstName + ' ' + age);
+const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${apiKey}`;
+
+
+$('.request').on('click', function () {
+  fetch(url)
+    .then(function (responseObj) {
+      const data = responseObj.json();
+      data.then(function (data) {
+        $('.output').html(`
+          <h2>${data.name}</h2>
+          <p>Temperature: ${data.main.temp}</p>
+          <p>Humidity: ${data.main.humidity}</p>
+          <p>Wind Speed: ${data.wind.speed}</p>
+      `)
+      })
+        .catch(function (error) {
+          console.log('Error: ', error);
+        });
+    });
+});
+
